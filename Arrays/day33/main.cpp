4a93dccac6ff7224e0641320ef44cb703ac34783
null-ptrx <ds331048@gmail.com>
2026-08-03 22:07:29 +0530

    subarraysWithXorK

--- Arrays/day33/main.cpp
+++ Arrays/day33/main.cpp
@@ -1,21 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int subarraysWithXorK(vector<int> &nums) {
+// int subarraysWithXorK(vector<int> &nums) {
+//     int k = 6;
+//     int count = 0;
+//     int n = nums.size();
+//     for (int i = 0; i < n; i++) {
+//         int xorr = 0;
+//         for (int j = i; j < n; j++) {
+//             xorr = xorr ^ nums[j];
+//             if (xorr == k) {
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+int subarraysWithXorK(vector<int> &nums)
+{
     int k = 6;
     int count = 0;
     int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        int xorr = 0;
-        for (int j = i; j < n; j++) {
-            xorr = xorr ^ nums[j];
-            if (xorr == k) {
-                count++;
-            }
-        }
+    map<int, int> mpp;
+    mpp[0] = 1;
+    int prefixXor = 0;
+    for (int i = 0; i < n; i++)
+    {
+        prefixXor ^= nums[i];
+        int needed = prefixXor ^ k;
+        count += mpp[needed];
+        mpp[prefixXor] += 1;
+     
     }
     return count;
 }
+
 int main()
 {
     vector<int> nums = { 4, 2, 2, 6, 4 };
